d1fb5f3f9ba6217b18fcdda86942d73bbadde9e0
Peeddrro <pedroh.araujo7784@gmail.com>
2025-03-07 22:07:08 +0000

    Atualizacao Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,33 +2,36 @@
 
 int main(){
 
+    //variaveis
     int populacao, populacao2, pontos_turisticos, pontos_turisticos2;
     char estado, estado2, codigo_carta[5], codigo_carta2[5], nome_cidade[50], nome_cidade2[50];
     float area, area2, pib, pib2;
-
+    float densidade, densidade2, pib_capita, pib_capita2;
+   
+    //entrada de dados
     printf("Carta 1\n");
 
     printf("Insira a letra que representa o Estado: ");
-    scanf(" %c", &estado); //O espaço antes do formato serve para o scanf ignorar os espaços em branco.
+    scanf(" %c", &estado); 
 
     printf("Insira o Código da Carta: ");
-    scanf(" %s", &codigo_carta); //Por algum motivo esse erro não aparece no VSCode local, apenas aqui no Git.
+    scanf("%s", codigo_carta); //Por algum motivo esse erro não aparece no VSCode local, apenas aqui no Git.
 
     printf("Insira o Nome da Cidade: ");
-    scanf(" %s", &nome_cidade);
+    scanf("%s", nome_cidade);
 
     printf("Insira o Número de Habitantes: ");
-    scanf(" %d", &populacao);
+    scanf("%d", &populacao);
 
     printf("Insira a Área da Cidade: ");
-    scanf(" %f", &area);
+    scanf("%f", &area);
 
     printf("Insira o PIB da Cidade: ");
-    scanf(" %f", &pib);
+    scanf("%f", &pib);
    
 
     printf("Insira quantos Pontos Turísticos existem na Cidade: ");
-    scanf(" %d", &pontos_turisticos);
+    scanf("%d", &pontos_turisticos);
  
 
 
@@ -39,25 +42,30 @@ int main(){
     scanf(" %c", &estado2);
 
     printf("Insira o Código da Carta: ");
-    scanf(" %s", &codigo_carta2);
+    scanf("%s", codigo_carta2);
 
     printf("Insira o Nome da Cidade: ");
-    scanf(" %s", &nome_cidade2);
+    scanf("%s", nome_cidade2);
 
     printf("Insira o Número de Habitantes: ");
-    scanf(" %d", &populacao2);
+    scanf("%d", &populacao2);
 
     printf("Insira a Área da Cidade: ");
-    scanf(" %f", &area2);
+    scanf("%f", &area2);
 
     printf("Insira o PIB da Cidade: ");
-    scanf(" %f", &pib2);
+    scanf("%f", &pib2);
     
     printf("Insira quantos Pontos Turísticos existem na Cidade: ");
-    scanf(" %d", &pontos_turisticos2);
+    scanf("%d", &pontos_turisticos2);
    
+    
+    densidade = (float) populacao / area;
+    densidade2 = (float) populacao2 / area2;
+    pib_capita = (float) pib / populacao;
+    pib_capita2 = (float) pib2 / populacao2;
 
-
+    //exibicao carta 1
     printf("\nCarta 1: \n");
     printf("Estado: %c\n", estado);
     printf("Codigo: %s\n", codigo_carta);
@@ -66,7 +74,10 @@ int main(){
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.2f bilhões de reais\n", pib);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos);
+    printf("Densidade Populacional: %.2f\n", densidade);
+    printf("PIB per Capita: %.2f\n", pib_capita);
 
+    //exibicao carta 2
     printf("\nCarta 2: \n");
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo_carta2);
@@ -75,6 +86,8 @@ int main(){
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f\n", densidade2);
+    printf("PIB per Capita: %.2f\n", pib_capita2);
 
     return 0;
 }
